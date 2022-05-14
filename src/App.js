115ed6379e1503fc06bdedcba1b04ea29bf76329
Adibittt2 <adityaverma00796@gmail.com>
2022-05-14 12:39:13 +0530

    calculator-app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
 
 // import logo from './logo.svg';
 import './App.css';
+import {add,sub,multi,div} from "./myFolder/calculater";
 
-function App() {
+// function App() {
 
-  let time = new Date(2022,15,6,22);
-  time = time.getHours();
-  let wish = "";
-  const sty = {};
-  if(time>=0 && time<12){
+//   let time = new Date(2022,15,6,22);
+//   time = time.getHours();
+//   let wish = "";
+//   const sty = {};
+//   if(time>=0 && time<12){
 
-    wish = "Good Morning";
-    sty.color = "blue";
-  }
-  else if(time>=12 && time<20){
-    wish = "Good Afternoon";
-    sty.color = "Green"; 
-  }
-  else{
-    wish = "Good Night";
-    sty.color = "Yellow";
-  }
+//     wish = "Good Morning";
+//     sty.color = "blue";
+//   }
+//   else if(time>=12 && time<20){
+//     wish = "Good Afternoon";
+//     sty.color = "Green"; 
+//   }
+//   else{
+//     wish = "Good Night";
+//     sty.color = "Yellow";
+//   }
 
-  return (
-    <div className="App">
-     <h1>Hello Sir, <span style = {sty}>{wish}</span>{time}</h1>
+//   return (
+//     <div className="App">
+//      <h1>Hello Sir, <span style = {sty}>{wish}</span>{time}</h1>
     
-    </div>
-  );
+//     </div>
+//   );
+// }
+
+
+// calculator app starts
+
+
+function App(){
+
+  return(
+  <ol>
+  
+  <li>This is add {add(6,7)}</li>
+  <li>This is sub {sub(6,7)}</li>
+  <li>This is multi {multi(6,7)}</li>
+  <li>This is div {div(6,7)}</li>
+  
+  </ol>
+  
+  ); 
+
 }
 
 
-// function add(a,b){
-//   let sum = a+b;
-//       return sum;
-//   }
-  
-//   function sub(a,b){
-//   let sub = a-b;
-//       return sub;
-  
-//   }
-  
+
   
 
 export default App;
 
-// export{add,sub};
