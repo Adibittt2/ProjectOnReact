7b753cd7d2c62d5234ba701eefefc0a5885a566a
Adibittt2 <adityaverma00796@gmail.com>
2022-05-18 11:02:58 +0530

    useState-hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import {add,sub,multi,div} from "./myFolder/calculater";
+// import {add,sub,multi,div} from "./myFolder/calculater";
+import slotFun from "./myFolder/slotMachine";
 
 // function App() {
 
@@ -35,24 +37,68 @@ import {add,sub,multi,div} from "./myFolder/calculater";
 // calculator app starts
 
 
+// function App(){
+
+//   return(
+//   <ol>
+  
+//   <li>This is add {add(6,7)}</li>
+//   <li>This is sub {sub(6,7)}</li>
+//   <li>This is multi {multi(6,7)}</li>
+//   <li>This is div {div(6,7)}</li>
+  
+//   </ol>
+  
+//   ); 
+
+// }
+
+
+// function App(){
+
+//   return(
+//  <>
+//   <div>
+//     <h1>{slotFun(3,5)}</h1>
+
+//     <h1>skfsj</h1>
+//   </div>
+//   </>
+//   ); 
+
+// }
+
+
 function App(){
 
-  return(
-  <ol>
-  
-  <li>This is add {add(6,7)}</li>
-  <li>This is sub {sub(6,7)}</li>
-  <li>This is multi {multi(6,7)}</li>
-  <li>This is div {div(6,7)}</li>
-  
-  </ol>
-  
-  ); 
+  // var count = 0;
+ const state = useState();
+  const [count,newfun] = useState(0);
 
+  const myfun = () =>{
+    return (
+      newfun(count+1)
+    )
+  }
+
+  
+  return (
+
+    <>
+    
+    <div>
+
+      <h1> {count}</h1>
+
+      <button onClick={myfun}>
+        Click Here
+      </button>
+
+    </div>
+
+    </>
+  )
 }
-
-
-
   
 
 export default App;
---
+++ src/myFolder/slotMachine.js
@@ -0,0 +1,41 @@
+
+// import React from "react";
+
+// function slotFun(){
+//     // let x = "a";
+//     // let y = "a";
+//     // let z = "a";
+
+//     // if((x == y) && (y == z)){
+//     //     return(
+//     //     <>
+//     //     <div><h1>Yes, all matched</h1></div>
+//     //     </>
+//     //     );
+//     // }
+//     // else{
+//     //     return(
+//     //     <>
+//     //     <div>
+//     //         <h1>No, not matched</h1>
+//     //     </div>
+        
+//     //     </>
+//     //     );
+//     // }
+
+//     return(
+
+
+//     );
+// }
+
+
+
+function slotFun(a,b){
+    let sum = a+b;
+let x = "abc :"
+    return sum;
+}
+
+export default slotFun;
